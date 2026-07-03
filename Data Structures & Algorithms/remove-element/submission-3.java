c137d46fedf042ba88acd0130a136e1d609a556e
Kristóf Szabó <kristofs577@gmail.com>
2026-07-03 18:06:22 +0200

    Add: remove-element - submission-3

---
+++ Data Structures & Algorithms/remove-element/submission-3.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != val) {
+                nums[count] = nums[index];
+                count++;
+            }
+        }
+        return count;
+    }
+}
